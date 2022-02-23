785025c023238989dfd80ba51bc751f8db6c3b0b
kmaluccio <kmaluccio@tamu.edu>
2022-02-23 13:55:25 -0600

    add html file and scraping python script

--- scraping.py
+++ scraping.py
@@ -24,8 +24,9 @@ def scrape_all():
     return data
 
 def mars_news(browser):
+    
     # Visit the mars nasa news site
-    url = 'https://redplanetscience.com'
+    url = 'https://data-class-mars.s3.amazonaws.com/Mars/index.html'
     browser.visit(url)
 
     # Optional delay for loading the page
@@ -42,13 +43,15 @@ def mars_news(browser):
         news_title = slide_elem.find('div',class_='content_title').get_text()
         # Use the parent element to find the paragraph text
         news_p = slide_elem.find('div',class_='article_teaser_body').get_text()
+
     except AttributeError:
         return None, None
+
     return news_title, news_p
 
 def featured_image(browser):
     # Visit URL
-    url = 'https://spaceimages-mars.com'
+    url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
     browser.visit(url)
 
     # Find and click the full image button
@@ -68,7 +71,7 @@ def featured_image(browser):
         return None
 
     # Use the base URL to create an absolute URL
-    img_url = f'https://spaceimages-mars.com/{img_url_rel}'
+    img_url = f'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/{img_url_rel}'
 
     return img_url
 
@@ -76,7 +79,7 @@ def mars_facts():
     # Add try/except for error handling
     try:
         # Use read_html to scrape the facts table into a dataframe
-        df = pd.read_html('https://galaxyfacts-mars.com')[0]
+        df = pd.read_html('https://data-class-mars-facts.s3.amazonaws.com/Mars_Facts/index.html')[0]
 
     except BaseException:
         return None
