39f1874ac45eca0db076dd1ca5e313068af6a312
kmaluccio <kmaluccio@tamu.edu>
2022-02-25 00:19:19 -0600

    updated versions of python script with correct for loop to get full url and title of images

--- Mission_to_Mars_Challenge.py
+++ Mission_to_Mars_Challenge.py
@@ -15,10 +15,12 @@ browser.visit(url)
 # Optional delay for loading the page
 browser.is_element_present_by_css('div.list_text',wait_time=1)
 
+
 # Convert the browser html to a soup object and then quit the browser
 html = browser.html
 news_soup = soup(html,'html.parser')
 slide_elem = news_soup.select_one('div.list_text')
+
 
 slide_elem.find('div',class_='content_title')
 
@@ -27,10 +29,10 @@ slide_elem.find('div',class_='content_title')
 news_title = slide_elem.find('div',class_='content_title').get_text()
 news_title
 
+
 # Use the parent element to find the paragraph text
 news_p = slide_elem.find('div',class_='article_teaser_body').get_text()
 news_p
-
 
 # ### JPL Space Images Featured Image
 
@@ -46,16 +48,13 @@ full_image_elem.click()
 html = browser.html
 img_soup = soup(html,'html.parser')
 
-
 # Find the relative image url
 img_url_rel = img_soup.find('img',class_='fancybox-image').get('src')
 img_url_rel
 
-
 # Use the base URL to create an absolute URL
 img_url = f'https://spaceimages-mars.com/{img_url_rel}'
 img_url
-
 
 # ### Mars Facts
 
@@ -66,7 +65,6 @@ df
 
 df.to_html()
 
-
 # ## D1: Scrape High-Resolution Mars' Hemisphere Images and Titles
 
 # ### Hemispheres
@@ -76,20 +74,29 @@ url = 'https://marshemispheres.com/'
 
 browser.visit(url)
 
-
 html = browser.html
 bs = soup(html,'html.parser')
 
-
 # 2. Create a list to hold the images and titles.
 hemisphere_image_urls = []
+
 # 3. Write code to retrieve the image urls and titles for each hemisphere.
 for i in range(4):
-    rel_img_path = bs.find_all('img',class_='thumb')[i]['src']
-    rel_img_title = bs.find_all('img')[i]['alt']
-    image_dict = {'image url':url+rel_img_path,'title':rel_img_title}
-    hemisphere_image_urls.append(image_dict)
-
+    # Create empty dictionary to store urls and titles for full images
+    hemispheres = {}
+    # click on hemisphere link
+    browser.links.find_by_partial_text('Enhanced')[i].click()
+    # navigate to full-resolution image page
+    html = browser.html
+    bs = soup(html,'html.parser')
+    rel_img_path = bs.find('ul').find('li').find('a')['href']
+    title = bs.find('div',class_='cover').find('h2').text
+    # retrieve the full resolution image URL string and title
+    hemispheres['img_url'] = url + rel_img_path
+    hemispheres['title'] = title
+    #navigate back to beginning to get the next hemisphere image
+    browser.back()
+    hemisphere_image_urls.append(hemispheres)
 
 # 4. Print the list that holds the dictionary of each image url and title.
 hemisphere_image_urls
