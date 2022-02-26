181b10c8eea84aff3a0de9c4bd8116015fd783e1
kmaluccio <kmaluccio@tamu.edu>
2022-02-25 23:47:07 -0600

    deliverable 2 code added with correct html and hemisphere images scraped from webpage

--- scraping.py
+++ scraping.py
@@ -11,19 +11,17 @@ def scrape_all():
     browser = Browser('chrome',**executable_path,headless=True)
 
     news_title, news_paragraph = mars_news(browser)
-    hemisphere_urls_titles = hemisphere_data(browser)
+    hemispheres = hemisphere_data(browser)
 
     # Run all scraping functions and store results in dictionary
     data = {"news_title": news_title,
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now(),
-        "image url":hemisphere_urls_titles[0]['image url'], "image title": hemisphere_urls_titles[0]['title'],
-        "image url":hemisphere_urls_titles[1]['image url'], "image title": hemisphere_urls_titles[1]['title'],
-        "image url":hemisphere_urls_titles[2]['image url'], "image title": hemisphere_urls_titles[2]['title'],
-        "image url":hemisphere_urls_titles[3]['image url'], "image title": hemisphere_urls_titles[3]['title']}
-    
+        "hemispheres": hemispheres,
+        # "img_url":[hemispheres[0]['img_url'],hemispheres[1]['img_url'],hemispheres[2]['img_url'], hemispheres[3]['img_url']],
+        # "title":[hemispheres[0]['title'],hemispheres[1]['title'],hemispheres[2]['title'],hemispheres[3]['title']],
+        "last_modified": dt.datetime.now()}
     # Stop webdriver and return data
     browser.quit()
     return data
@@ -97,10 +95,11 @@ def mars_facts():
     return df.to_html(classes="table table-striped")
 
 def hemisphere_data(browser):
-    # 1. Visit url using browser
+    # 1. Use browser to visit the URL 
     url = 'https://marshemispheres.com/'
+
     browser.visit(url)
-    
+
     html = browser.html
     bs = soup(html,'html.parser')
 
@@ -109,10 +108,21 @@ def hemisphere_data(browser):
 
     # 3. Write code to retrieve the image urls and titles for each hemisphere.
     for i in range(4):
-        rel_img_path = bs.find_all('img',class_='thumb')[i]['src']
-        rel_img_title = bs.find_all('img')[i]['alt']
-        image_dict = {'image url':url+rel_img_path,'title':rel_img_title}
-        hemisphere_image_urls.append(image_dict)
+        # Create empty dictionary to store urls and titles for full images
+        hemispheres = {}
+        # click on hemisphere link
+        browser.links.find_by_partial_text('Enhanced')[i].click()
+        # navigate to full-resolution image page
+        html = browser.html
+        bs = soup(html,'html.parser')
+        rel_img_path = bs.find('ul').find('li').find('a')['href']
+        title = bs.find('div',class_='cover').find('h2').text
+        # retrieve the full resolution image URL string and title
+        hemispheres['img_url'] = url + rel_img_path
+        hemispheres['title'] = title
+        #navigate back to beginning to get the next hemisphere image
+        browser.back()
+        hemisphere_image_urls.append(hemispheres)
 
     # 4. Print the list that holds the dictionary of each image url and title.
     return hemisphere_image_urls
